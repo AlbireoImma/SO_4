a7b474dbc565570e583ce52106a15b825b231e32
AlbireoImma <faam1612@gmail.com>
2018-12-12 21:12:14 -0300

    [WIP] Modularizacion

--- tienda.py
+++ tienda.py
@@ -4,11 +4,14 @@ import time
 
 lock = Sem(1)
 bano = Sem(1)
-Fila_Clientes = []
-Clientes_Tienda = 0
-numero = 1
+Clientes_Esperando = list()
+tienda = Tienda()
 
-class Meson(Thread):
+for i in range(50):
+    Clientes_Esperando.append(Cliente(i))
+
+
+class Atencion(Thread):
     def __init__(self, val):
         # Constructor
         Thread.__init__(self)
@@ -16,6 +19,13 @@ class Meson(Thread):
         self.clientes = []
         self.atendidos = 0
         self.en_cola = 0
+
+    def insertCliente(self,cliente):
+        self.clientes.append(cliente)
+        self.en_cola += 1
+
+    def get_cola(self):
+        return self.en_cola
 
     def run(self):
         for a in range(5):
@@ -40,23 +50,42 @@ class Cliente():
         self.estado = 0 # 0:Espera 1:Eligiendo 2:Pagando 3:Saliendo
 
 # Clase representativa de la tienda
-class Tienda():
+class Tienda(Thread):
     def __init__(self):
         self.clientes = []
         self.espacio = 30
         self.abierta = 0
+        self.id = 0
+    def run(self):
+        while Clientes_Esperando > 0:
+            while espacio > 0:
+                self.clientes.append(id)
+                Clientes_Esperando -= 1
+                min = 100
+                for meson in atencion:
+                    if min > meson.get_cola:
+                        min = meson.nro_meson - 1
+                atencion[min].insertCliente(id)
+                id += 1
+            time.sleep(2)
+
     def addCliente(cliente):
-        self.clientes.append(cliente)
-        self.espacio -= 1
+        if espacio > 0:
+            self.clientes.append(cliente)
+            self.espacio -= 1
+    def removeCliente(id):
+        if id in self.clientes:
+            self.clientes.remove(id)
+
 
 def main():
-    mesones = list()
+    atencion = list()
     for i in range(5):
-        mesones.append(Meson(i+1))
+        atencion.append(Atencion(i+1))
     print("[Main] Iniciando mesones")
-    for meson in mesones:
+    for meson in atencion:
         meson.start()
-    for meson in mesones:
+    for meson in atencion:
         meson.join()
     print("[Main] All join")
     return
