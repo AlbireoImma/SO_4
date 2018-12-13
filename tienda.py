39144409c5c34b237619cd8b8d95466f2bbfc427
AlbireoImma <faam1612@gmail.com>
2018-12-12 23:02:26 -0300

    [WIP] Modularizacion

--- tienda.py
+++ tienda.py
@@ -2,23 +2,24 @@ from threading import BoundedSemaphore as Sem
 from threading import Thread
 import time
 
-lock = Sem(1)
-bano = Sem(1)
-Clientes_Esperando = list()
-tienda = Tienda()
-
-for i in range(50):
-    Clientes_Esperando.append(Cliente(i))
-
+def get_posmin(atencion):
+    i = 0
+    min = len(atencion[0].clientes)
+    for item in atencion:
+        if min > len(item.clientes):
+            min = len(item.clientes)
+            i = item.nro_meson - 1
+    return i
 
 class Atencion(Thread):
-    def __init__(self, val):
+    def __init__(self, val, tienda):
         # Constructor
         Thread.__init__(self)
         self.nro_meson = val
         self.clientes = []
         self.atendidos = 0
         self.en_cola = 0
+        self.tienda = tienda
 
     def insertCliente(self,cliente):
         self.clientes.append(cliente)
@@ -28,14 +29,20 @@ class Atencion(Thread):
         return self.en_cola
 
     def run(self):
-        while(tienda.abierta == 1):
+        while(len(Clientes_Esperando) > 0):
             while(len(self.clientes) > 0):
                 time.sleep(2)
                 self.atendidos += 1
                 print("[Thread {}] Clientes atendidos: {}, Cliente actual: {}".format(self.nro_meson,self.atendidos,self.clientes[0]))
+                to_erase = self.clientes[0]
                 del self.clientes[0]
-                print("[Thread "+str(self.nro_meson)+"] Clientes esperando -> ",self.clientes)
-            print("[Thread %d] Trabajo cumplido" % self.nro_meson)
+                print("[Thread " + str(self.nro_meson) + "] Clientes esperando -> ",self.clientes)
+                print("[Thread %d] Trabajo cumplido" % self.nro_meson)
+                time.sleep(3)
+                remover.acquire()
+                self.tienda.clientes.remove(to_erase)
+                remover.release()
+        print("[Thread %d] Meson Cerrado" % self.nro_meson)
         return
 
 # Clase representativa de un cliente
@@ -47,41 +54,55 @@ class Cliente():
 # Clase representativa de la tienda
 class Tienda(Thread):
     def __init__(self):
+        Thread.__init__(self)
         self.clientes = []
         self.espacio = 30
         self.abierta = 0
         self.id = 0
+        self.cajas = []
+        self.atencion = []
+        self.bano = True
     def run(self):
-        while Clientes_Esperando > 0:
-            while espacio > 0:
-                self.clientes.append(id)
-                Clientes_Esperando -= 1
+        print("[Tienda] Espacio: ",self.espacio)
+        while (len(Clientes_Esperando) > 0):
+            while self.espacio > 0:
+                print("[Tienda] Espacio: ",self.espacio)
+                self.clientes.append(Clientes_Esperando[0].nro_cliente)
                 min = 100
-                for meson in atencion:
-                    if min > meson.get_cola:
-                        min = meson.nro_meson - 1
-                atencion[min].insertCliente(id)
-                id += 1
+                self.atencion[get_posmin(self.atencion)].clientes.append(Clientes_Esperando[0].nro_cliente)
+                self.espacio -= 1
+                del Clientes_Esperando[0]
             time.sleep(2)
+            print("[Tienda] Espacio: ",self.espacio)
+            print("[Tienda] Clientes: ",self.clientes)
+            print("[Tienda] Clientes Esperando: ",len(Clientes_Esperando))
+            self.espacio = 30 - len(self.clientes)
+            if Clientes_Esperando == 0:
+                self.abierta = 0
+        print("[Tienda] Listo")
+        return
 
-    def addCliente(cliente):
-        if espacio > 0:
-            self.clientes.append(cliente)
-            self.espacio -= 1
-    def removeCliente(id):
-        if id in self.clientes:
-            self.clientes.remove(id)
+lock = Sem(1)
+bano = Sem(1)
+remover = Sem(1)
+Clientes_Esperando = list()
 
+for i in range(50):
+    Clientes_Esperando.append(Cliente(i))
 
 def main():
+    tienda = Tienda()
     atencion = list()
     for i in range(5):
-        atencion.append(Atencion(i+1))
+        atencion.append(Atencion(i+1,tienda))
+    print("[Main] Abriendo Tienda")
+    tienda.abierta = 1
     print("[Main] Iniciando mesones")
     for meson in atencion:
         meson.start()
-    for meson in atencion:
-        meson.join()
+    print("[Main] Iniciando Entrada de clientes")
+    tienda.atencion = atencion
+    tienda.start()
     print("[Main] All join")
     return
 
