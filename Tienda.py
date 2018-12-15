71be7ec7b599e10d07532aa5c88ad54eb5692b50
AlbireoImma <faam1612@gmail.com>
2018-12-15 18:21:00 -0300

    [WIP] Tinda.py

--- Tienda.py
+++ Tienda.py
@@ -20,4 +20,39 @@ class Tienda(Thread):
     def run(self):
         while (len(Clientes_Afuera) > 0):
             while (self.espacio > 0):
-                
+                i = 0
+                j = 0
+                minimal = 1000
+                for meson in Atencion:
+                    if len(meson.clientes) <= minimal:
+                        minimal = len(meson.clientes)
+                        j = i
+                    i+=1
+                Atencion[j].clientes.append(Clientes_Afuera[0])
+                Atencion[j].clientes[-1].estado += 1
+                del Clientes_Afuera[0]
+                self.espacio -= 1
+            time.sleep(5)
+        return
+
+
+class Atender(Thread):
+    def __init__(self,val):
+        Thread.__init__(self)
+        self.clientes = list()
+        self.atendidos = 0
+        self.baño = 0
+        self.numero = val
+    def run(self):
+        while(len(Clientes_Afuera) > 0):
+            pass
+        return
+
+
+
+Atencion = list()
+for a in range(5):
+    Atencion.append(Atender(a+1))
+Clientes_Afuera = list()
+for a in range(50):
+    Clientes_Afuera.append(Cliente(a+1))
