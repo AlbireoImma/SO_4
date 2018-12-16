3de2c04c2c64442481193fb3e9fde39d61731408
AlbireoImma <faam1612@gmail.com>
2018-12-16 19:55:02 -0300

    Finish Him...

--- Tienda.py
+++ Tienda.py
@@ -1,6 +1,10 @@
 from threading import BoundedSemaphore as Sem
 from threading import Thread
 import time
+from time import gmtime, strftime
+
+def getTime():
+    return strftime("[%d-%m-%Y %H:%M:%S]", gmtime())
 
 class Cliente(Thread):
     def __init__(self,val):
@@ -11,7 +15,7 @@ class Cliente(Thread):
         while(self.estado < 3):
             time.sleep(4)
             # print("[Cliente "+str(self.id)+"] En estado: "+str(self.estado))
-        # print("[Cliente "+str(self.id)+"] Se retira de la tienda")
+        log_clientes.write(getTime()+"[Cliente "+str(self.id)+"] Se retira de la tienda\n")
         return
 
 class Tienda(Thread):
@@ -20,7 +24,7 @@ class Tienda(Thread):
         self.espacio = 30
     def run(self):
         while (len(Clientes_Afuera) > 0):
-            while (self.espacio > 0):
+            while (self.espacio > 0 and len(Clientes_Afuera) > 0):
                 i = 0
                 j = 0
                 minimal = 1000
@@ -29,28 +33,29 @@ class Tienda(Thread):
                         minimal = len(meson.clientes)
                         j = i
                     i+=1
-                print("[Tienda] Cliente "+str(Clientes_Afuera[0].id)+" asignado a meson "+str(Atencion[j].numero))
+                log_funcionarios.write(getTime()+"[Tienda] Cliente "+str(Clientes_Afuera[0].id)+" asignado a meson "+str(Atencion[j].numero)+"\n")
                 Clientes_Adentro.append(Clientes_Afuera[0])
                 Clientes_Adentro[-1].estado += 1
                 Atencion[j].clientes.append(Clientes_Afuera[0])
                 del Clientes_Afuera[0]
                 Clientes_Adentro[-1].start()
+                log_clientes.write(getTime()+"[Cliente "+str(Clientes_Adentro[-1].id)+"] Ha entrado a la tienda\n")
                 self.espacio = 30 - len(Clientes_Adentro)
             self.espacio = 30 - len(Clientes_Adentro)
-            print("[Tienda] Durmiendo...")
-            print("[Tienda] Espacio disponible en la tienda: "+str(self.espacio))
-            print("[Tienda] Gente esperando afuera: "+str(len(Clientes_Afuera)))
+            print(getTime()+"[Tienda] Espacio disponible en la tienda: "+str(self.espacio))
+            print(getTime()+"[Tienda] Gente esperando afuera: "+str(len(Clientes_Afuera)))
             time.sleep(5)
         return
 
 
 class Atender(Thread):
-    def __init__(self,val):
+    def __init__(self,val,val2):
         Thread.__init__(self)
         self.clientes = list()
         self.atendidos = 0
-        self.baño = 0
+        self.cola_baño = 0
         self.numero = val
+        self.id = val2
     def run(self):
         while(len(Clientes_Afuera) > 0):
             while(len(self.clientes) > 0):
@@ -58,64 +63,100 @@ class Atender(Thread):
                 # print("[Meson "+str(self.numero)+"] Atendiendo a cliente "+str(ident))
                 i = 0
                 j = 0
+                mov_caja.acquire()
                 for a in Clientes_Adentro:
                     if ident == a.id:
                         j = i
                         break
                     i +=1
                 Clientes_Adentro[j].estado += 1
-                self.atendidos += 1
-                print("[Meson "+str(self.numero)+"] Cliente "+str(ident)+" atendido")
-                print("[Meson "+str(self.numero)+"] Clientes atendidos: "+str(self.atendidos))
-                print("[Meson "+str(self.numero)+"] Clientes por atender: "+str(len(self.clientes)))
-                mov_caja.acquire()
+                log_funcionarios.write(getTime()+"[Meson "+str(self.numero)+"] Cliente "+str(self.clientes[0].id)+" siendo atendido\n")
+                log_clientes.write(getTime()+"[Cliente "+str(self.clientes[0].id)+"] En meson de informacion numero "+str(self.numero)+"\n")
+                # print("[Meson "+str(self.numero)+"] Clientes atendidos: "+str(self.atendidos))
+                print(getTime()+"[Meson "+str(self.numero)+"] Clientes por atender: "+str(len(self.clientes)))
                 if len(Cajas[0].clientes) > len(Cajas[1].clientes):
-                    Clientes_Adentro[j].estado += 1
                     Cajas[1].clientes.append(Clientes_Adentro[j])
                 else:
-                    Clientes_Adentro[j].estado += 1
-                    Cajas[0].clientes.append(Clientes_Adentro[j])
+                    try:
+                        Cajas[0].clientes.append(Clientes_Adentro[j])
+                    except:
+                        pass
                 mov_caja.release()
                 del self.clientes[0]
                 time.sleep(3)
+                if self.atendidos == 4:
+                    log_funcionarios.write(getTime()+"[Meson "+str(self.numero)+"] Revisando disponibilidad baño\n")
+                    if self.id not in baño.cola:
+                        baño.cola.append(self.id)
+                    if baño.cola[0] == self.id:
+                        log_funcionarios.write(getTime()+"[Meson "+str(self.numero)+"] Usando baño\n")
+                        time.sleep(2)
+                        del baño.cola[0]
+                        self.atendidos = 0
+                else:
+                    self.atendidos += 1
+
         return
 
 class Caja(Thread):
-    def __init__(self,val):
+    def __init__(self,val,val2):
         Thread.__init__(self)
         self.clientes = list()
         self.atendidos = 0
-        self.baño = 0
+        self.cola_baño = 0
         self.numero = val
+        self.id = val2
     def run(self):
         while(len(Clientes_Afuera) > 0):
             # print("[Caja"+str(self.numero)+"] Tiene "+str(len(self.clientes))+" en cola")
             while len(self.clientes) > 0:
-                print("[Caja "+str(self.numero)+"] Tiene "+str(len(self.clientes))+" en cola")
-                time.sleep(2)
+                # print("[Caja "+str(self.numero)+"] Clientes atendidos: "+str(self.atendidos))
+                print(getTime()+"[Caja "+str(self.numero)+"] Tiene "+str(len(self.clientes))+" en cola")
+                log_funcionarios.write(getTime()+"[Caja "+str(self.numero)+"] Atendiendo a cliente "+str(self.clientes[0].id)+"\n")
+                time.sleep(5)
                 ident = self.clientes[0].id
                 i = 0
                 j = 0
+                mov_afuera.acquire()
                 for a in Clientes_Adentro:
                     if a.id == ident:
                         j = i
                         break
                     i += 1
                 Clientes_Adentro[j].estado += 10
-                mov_afuera.acquire()
+                log_funcionarios.write(getTime()+"[Caja "+str(self.numero)+"] Cliente "+str(self.clientes[0].id)+" atendido\n")
+                log_clientes.write(getTime()+"[Cliente "+str(self.clientes[0].id)+"] Actualmente en caja\n")
                 del self.clientes[0]
                 del Clientes_Adentro[j]
                 mov_afuera.release()
+                if self.atendidos == 4:
+                    log_funcionarios.write(getTime()+"[Caja "+str(self.numero)+"] Revisando disponibilidad baño\n")
+                    if self.id not in baño.cola:
+                        baño.cola.append(self.id)
+                    if baño.cola[0] == self.id:
+                        log_funcionarios.write(getTime()+"[Caja "+str(self.numero)+"] Usando baño\n")
+                        time.sleep(2)
+                        del baño.cola[0]
+                        self.atendidos = 0
+                else:
+                    self.atendidos += 1
         return
 
+class Baño():
+    def __init__(self):
+        self.cola = list()
 
 
+
+ident = 1
 Atencion = list()
 for a in range(5):
-    Atencion.append(Atender(a+1))
+    Atencion.append(Atender(a+1,ident))
+    ident += 1
 Cajas = list()
 for a in range(2):
-    Cajas.append(Caja(a+1))
+    Cajas.append(Caja(a+1,ident))
+    ident += 1
 Clientes_Afuera = list()
 for a in range(50):
     Clientes_Afuera.append(Cliente(a+1))
@@ -124,6 +165,10 @@ mov_caja = Sem(1)
 mov_meson = Sem(1)
 mov_afuera = Sem(1)
 lock_print = Sem(1)
+lock_baño = Sem(1)
+baño = Baño()
+log_funcionarios = open("funcionarios.txt","a")
+log_clientes = open("clientes.txt","a")
 
 def main():
     print("Main")
@@ -139,7 +184,10 @@ def main():
     for caja in Cajas:
         caja.join()
     tienda.join()
-    print("All join")
-    return
+    print(getTime()+"[Tienda] Todos los clientes atendidos")
+    print(getTime()+"[Tienda] Cerrando tienda...")
+    print(getTime()+"[Tienda] Tienda cerrada")
+    exit()
 
-main()
+if __name__ == "__main__":
+    main()
